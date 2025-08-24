c1a0e242464d40caf9e81761f0d98b8838c2bf5c
ShigeoYugawa <shigeo.yugawa.dev.2@gmail.com>
2025-08-24 20:28:59 +0900

    i18n対応追加

--- task_manager/accounts/admin.py
+++ task_manager/accounts/admin.py
@@ -1,6 +1,6 @@
 # task_manager/accounts/admin.py
 
-from django.utils.translation import gettext_lazy as _
+from django.utils.translation import gettext as _
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
--- task_manager/accounts/forms.py
+++ task_manager/accounts/forms.py
@@ -3,7 +3,6 @@
 from __future__ import annotations
 from django import forms
 from django.contrib import admin
-#from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.forms import ReadOnlyPasswordHashField
 from .models import CustomUser
 
